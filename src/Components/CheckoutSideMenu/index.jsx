6dd8081e008c5333159ade6125e1fc2028ff864f
steeltx <realsteel.gnu@gmail.com>
2024-01-19 23:18:50 -0600

    Funcionalidad para evitar productos duplicados en el carrito y cambio de icono

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -8,15 +8,13 @@ const CheckoutSideMenu = () => {
     
     const context = useContext(ShoppingCartContext);
 
-    console.log(context.cartProducts);
-
     return (
         <aside className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-slide-menu flex-col fixed right-0 border border-black rounded bg-white`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-bold text-xl'>My Order</h2>
                 <XMarkIcon className="h-6 w-6 text-black cursor-pointer" onClick={() => context.closeCheckoutSideMenu()} />
             </div>
-            <div className='px-6'>
+            <div className='px-6 overflow-y-scroll'>
                 {
                     context.cartProducts.map(product => (
                         <OrderCard
