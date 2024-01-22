2098dd8e7b073510cce889ec6322a6c891a6a933
steeltx <realsteel.gnu@gmail.com>
2024-01-21 18:02:03 -0600

    Diseño para vista de ordenes

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,13 +1,31 @@
+import { CalendarDaysIcon, ChevronRightIcon, ListBulletIcon } from "@heroicons/react/24/solid";
 
 const OrdersCard = ( { totalPrice, totalProducts } ) => {
 
     return(
-        <div className='flex justify-between items-center mb-3 max-w-sm rounded overflow-hidden shadow-md p-1 border border-black'>
-            <p>
-                <span>{ new Date().toLocaleDateString()}</span>
-                <span>{totalProducts}</span>
-                <span>{totalPrice}</span>
-            </p>
+        <div className='flex justify-between items-center mb-4 border border-black rounded-lg w-80 p-4 shadow-md'>
+            <div className='flex justify-between w-full'>
+                <div className='flex flex-col'> 
+                <p className='flex'>
+                    <CalendarDaysIcon 
+                        className='h-6 w-6 text-black cursor-pointer'>
+                    </CalendarDaysIcon>
+                    <span className='font-medium px-2'>{ new Date().toLocaleDateString()}</span>
+                </p>
+                <p className='flex'>
+                    <ListBulletIcon 
+                        className='h-6 w-6 text-black cursor-pointer'>
+                    </ListBulletIcon>
+                    <span className='font-medium px-2'>{totalProducts} articulos</span>
+                </p>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <span className='font-bold text-2xl text-blue-600'>${totalPrice}</span>
+                    <ChevronRightIcon 
+                        className='h-6 w-6 text-black cursor-pointer'>
+                    </ChevronRightIcon>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -10,8 +10,8 @@ function MyOders() {
 
     return (
         <Layout>
-            <div className='flex items-center justify-center relative w-80'>
-                <h1>My Orders</h1>
+            <div className='flex items-center justify-center relative w-80 mb-4'>
+                <h1 className='font-medium text-xl'>My Orders</h1>
             </div>
             {
                 context.order.map((order, index) => (
